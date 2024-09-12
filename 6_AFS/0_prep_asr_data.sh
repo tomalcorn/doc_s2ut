032dd96ea667a1e9a7fad0ee68f5f4332d4a9d2c
Tom Alcorn <s2517781@ed.ac.uk>
2024-09-12 19:41:56 +0100

    fixed prep_asr_data.sh

--- 6_AFS/0_prep_asr_data.sh
+++ 6_AFS/0_prep_asr_data.sh
@@ -2,9 +2,11 @@ AFS_DATA_ROOT=""
 AUDIOROOT=""
 TRANSCRIPTIONS=""
 VOCAB_SIZE=10000
+# Comma separated list eg "train,dev,test"
+SPLITS=""
 
-cd /Users/tomalcorn/Documents/University/pg/diss/fairseq
+cd /fairseq
 
 PYTHONPATH=. python examples/speech_to_text/1_prep_asr_data.py --output-root $AFS_DATA_ROOT \
     --audio-root $AUDIOROOT --transcription-file $TRANSCRIPTIONS \
-    --vocab-type unigram --vocab-size $VOCAB_SIZE
+    --vocab-type unigram --vocab-size $VOCAB_SIZE --splits $SPLITS
